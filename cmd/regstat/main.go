c8eb4ec10382c7784be92c45b9ad30345120b4b4
vleurgat <compston@gmail.com>
2018-11-09 23:01:20 +0000

    add Dockerfile; set better Postgres default config

--- cmd/regstat/main.go
+++ cmd/regstat/main.go
@@ -12,7 +12,7 @@ func main() {
 	var dockerConfigFile string
 	var equivRegistriesFile string
 	flag.StringVar(&port, "port", "3333", "the port number to listen on")
-	flag.StringVar(&pgConnStr, "pg-conn-str", "", "the Postgres connect string, e.g. \"host=host port=1234 user=user password=pw ...\"")
+	flag.StringVar(&pgConnStr, "pg-conn-str", "\"host=localhost port=5432 user=postgres sslmode=disable\"", "the Postgres connect string, e.g. \"host=host port=1234 user=user password=pw ...\"")
 	flag.StringVar(&dockerConfigFile, "docker-config", "", "the path to the Docker registry config.json file, used to obtain login credentials")
 	flag.StringVar(&equivRegistriesFile, "equiv-registries", "", "the path to the equiv-registries.json file, used to combine equivalent registries")
 	flag.Parse()
